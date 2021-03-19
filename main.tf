5d5e95813505f2c4d53b60c5c26dd33195e5dbfa
k.inoue <k.inoue@remcorp.jp>
2021-03-19 21:37:39 +0900

    add key pair

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+# Variables import form terraform.tfvars
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "key_name" {}
+variable "public_key_path" {}
+
 # Provider
 provider "aws" {                  // use AWS
   access_key = var.aws_access_key // this information is credential so use the variables on ./terraform.tfvars
@@ -11,15 +17,14 @@ resource "aws_vpc" "myVPC" { // aws vpc which named "myVPC"
   instance_tenancy     = "default" // "defalut" - The instance runs on the shared hardware.
   enable_dns_support   = "true"
   enable_dns_hostnames = "false"
-  tags {
+  tags = {
     Name = "myVPC" // name of VPC
   }
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "myGW" { // aws internet gateway which named "myGW"
-  vpc_id     = aws_vpc.myVPC.id          // refer the id of vpc resource ${resource.resourceName.id}
-  depends_on = aws_vpc.myVPC             // Clarify that it depends on "myVPC" *not required
+  vpc_id = aws_vpc.myVPC.id              // refer the id of vpc resource ${resource.resourceName.id}
 }
 
 # Public Subnet
@@ -55,6 +60,12 @@ resource "aws_security_group" "admin" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -63,31 +74,36 @@ resource "aws_security_group" "admin" {
   }
 }
 
+resource "aws_key_pair" "auth" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
 # EC2 Instance
 resource "aws_instance" "cm-test" {
-  ami           = var.images.ap-northeast-1
+  ami           = "ami-0f27d081df46f326c"
   instance_type = "t2.micro"
-  key_name      = "cm-yawata.yutaka"
+  key_name      = aws_key_pair.auth.id
   vpc_security_group_ids = [
-    "${aws_security_group.admin.id}"
+    aws_security_group.admin.id
   ]
   subnet_id                   = aws_subnet.public-a.id
   associate_public_ip_address = "true"
-  root_block_device = {
+  root_block_device {
     volume_type = "gp2"
     volume_size = "20"
   }
-  ebs_block_device = {
+  ebs_block_device {
     device_name = "/dev/sdf"
     volume_type = "gp2"
     volume_size = "100"
   }
-  tags {
+  tags = {
     Name = "cm-test"
   }
 }
 
 # Out Put Result
-output "public ip of cm-test" {
+output "public_ip_of_cm-test" {
   value = aws_instance.cm-test.public_ip
 }
